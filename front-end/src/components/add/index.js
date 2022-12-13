d5fa176c78cb9431b20dfc20835e7e3b86539824
francoisdouillet <francois.douillet@sfr.fr>
2022-12-13 15:14:55 +0100

    some amelioration

--- front-end/src/components/add/index.js
+++ front-end/src/components/add/index.js
@@ -1,39 +1,41 @@
 import NavBar from "../navbar/Navbar";
 import { useState } from "react";
-import ArrowForwardIcon from "@mui/icons-material/ArrowForward";
 import ArrowBackIcon from "@mui/icons-material/ArrowBack";
 import { Button } from "@mui/material";
 import Role from "./Role";
 import Mychamp from "./Mychamp";
 import Hischamp from "./HisChamp";
+import Difficulty from "./Difficulty";
 
 function Add() {
   const [page, setPage] = useState(0);
-  const [disabled, setDisabled] = useState(true);
   const [formData, setFormData] = useState({
     role: "",
     myChamp: "",
     hisChamp: "",
     difficulty: "",
+    commentary: "",
   });
 
   console.log(formData)
 
   const PageDisplay = () => {
     if (page === 0) {
-      return <Role formData={formData} setFormData={setFormData} setDisabled={setDisabled} page={page} setPage={setPage}/>;
+      return <Role formData={formData} setFormData={setFormData} page={page} setPage={setPage}/>;
     } else if (page === 1) {
-      return <Mychamp formData={formData} setFormData={setFormData} setDisabled={setDisabled} page={page} setPage={setPage}/>;
+      return <Mychamp formData={formData} setFormData={setFormData} page={page} setPage={setPage}/>;
     } else if (page === 2) {
-      return <Hischamp formData={formData} setFormData={setFormData} setDisabled={setDisabled} page={page} setPage={setPage}/>
+      return <Hischamp formData={formData} setFormData={setFormData} page={page} setPage={setPage}/>
+    } else if (page === 3) {
+      return <Difficulty formData={formData}/>
     }
   };
 
   return (
     <div className="formadd">
       <PageDisplay />
-      {page === 0 ? (
-        <Button></Button>
+      {page === 3 ? (
+        <></>
       ) : (
         <>
           <Button>
