7ad903be2240673e9fdd74e9c803250f4e65538b
francoisdouillet <francois.douillet@sfr.fr>
2023-03-27 15:09:52 +0200

    Update ChampionSheet.js

--- front-end/src/components/champions/championsheet/ChampionSheet.js
+++ front-end/src/components/champions/championsheet/ChampionSheet.js
@@ -9,36 +9,6 @@ import SheetSpell from "./SheetSpell";
 const ChampionSheet = () => {
   const sheetId = useParams();
   const sheet = GetOneSheet({ id: sheetId.id });
-<<<<<<< HEAD
-  const navigate = useNavigate()
-
-  const [anchorEl, setAnchorEl] = useState(null);
-
-  const handleClick = (event) => {
-    setAnchorEl(event.currentTarget);
-  };
-
-  const handleClose = () => {
-    setAnchorEl(null);
-  };
-
-  async function onDelete() {
-    if(window.confirm('Voulez-vous supprimmez cette fiche ?')) {
-      try {
-        const response = await axios.delete(`http://localhost:3000/api/champion/sheet/${sheetId.id}`);
-        console.log(response);
-        alert("Fiche de champion supprimé !");
-        navigate('/champions')
-      } catch (error) {
-        console.error(error);
-      }
-    }
-  }
-  console.log('test')
-=======
-  console.log(sheet)
-
->>>>>>> parent of 5c1ca49 (back end + front delete sheet)
   return (
     <div className="champions">
       <img
