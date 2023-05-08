91ad114b723c7f8f97421454dbec48d06dd49f28
francoisdouillet <francois.douillet@sfr.fr>
2023-05-08 21:53:45 +0200

    test

--- front-end/src/components/champions/championsheet/Modify/ModifyItem.js
+++ front-end/src/components/champions/championsheet/Modify/ModifyItem.js
@@ -1,20 +1,46 @@
-import { Button } from "@mui/material";
+// import { Button, Modal } from "@mui/material";
+// import CloseIcon from "@mui/icons-material/Close";
+// import ChooseItems from "../../form/ChooseItems";
+
+// const ModifyItem = ({isOpen, setIsOpen, sheet}) => {
+//   const formData = sheet
+//   return (
+//     <div className={`champions__modify ${isOpen ? "open" : ""}`}>
+//       <Button
+//         onClick={() => setIsOpen(false)}
+//         className="champions__modify--close"
+//       >
+//         <CloseIcon fontSize="large" />
+//       </Button>
+//       <ChooseItems formData={formData} modify={true}/>
+//     </div>
+//   );
+// };
+
+// export default ModifyItem
+
+import { useState } from "react";
+import { Button, Modal } from "@mui/material";
 import CloseIcon from "@mui/icons-material/Close";
 import ChooseItems from "../../form/ChooseItems";
 
 const ModifyItem = ({isOpen, setIsOpen, sheet}) => {
-  const formData = sheet
+  const formData = sheet;
+  
+  const handleClose = () => {
+    setIsOpen(false);
+  };
+
   return (
-    <div className={`champions__modify ${isOpen ? "open" : ""}`}>
-      <Button
-        onClick={() => setIsOpen(false)}
-        className="champions__modify--close"
-      >
-        <CloseIcon fontSize="large" />
-      </Button>
-      <ChooseItems formData={formData} modify={true}/>
-    </div>
+    <Modal open={isOpen} onClose={handleClose}>
+      <div className="champions__modify">
+        <Button onClick={handleClose} className="champions__modify--close">
+          <CloseIcon fontSize="large" />
+        </Button>
+        <ChooseItems formData={formData} modify={true} />
+      </div>
+    </Modal>
   );
 };
 
-export default ModifyItem
+export default ModifyItem;
