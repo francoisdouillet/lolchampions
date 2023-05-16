b520ce34c3d1fb9b502e7dc70fb8bd05a52336c9
francoisdouillet <francois.douillet@sfr.fr>
2023-05-16 10:45:06 +0200

    Update UserFindMatches.js

--- front-end/src/components/user/UserFindMatches.js
+++ front-end/src/components/user/UserFindMatches.js
@@ -8,23 +8,30 @@ const UserFindMatches = () => {
     console.log(username);
 
     try {
-      const response = await fetch(
-        `https://euw1.api.riotgames.com/lol/summoner/v4/summoners/by-name/${username}?api_key=RGAPI-4b544145-49c2-424d-9cc0-2bf92f56a5ca`
-      );
-      const data = await response.json();
-      const summonerId = data.puuid;
+      // const response = await fetch(
+      //   `https://euw1.api.riotgames.com/lol/summoner/v4/summoners/by-name/${username}?api_key=RGAPI-4b544145-49c2-424d-9cc0-2bf92f56a5ca`
+      // );
+      // const data = await response.json();
+      // console.log(data)
+      // const summonerId = data.accountId;
 
-      const matchResponse = await fetch(
-        `https://europe.api.riotgames.com/lol/match/v5/matches/by-puuid/${summonerId}/ids?start=0&count=1&api_key=RGAPI-4b544145-49c2-424d-9cc0-2bf92f56a5ca`
-      );
-      const matchData = await matchResponse.json();
-      console.log(matchData);
+      const test = await fetch (
+        `https://euw1.api.riotgames.com/lol/league/v4/entries/by-summoner/xwi1HQDj6cWSsK8QGBRwl6OiW_P53TftFuBbat52R1xfoQ?api_key=RGAPI-4b544145-49c2-424d-9cc0-2bf92f56a5ca`
+      )
+      const dataTest = await test.json()
+      console.log(dataTest)
+      
+      // const matchResponse = await fetch(
+      //   `https://europe.api.riotgames.com/lol/match/v5/matches/by-puuid/${summonerId}/ids?start=0&count=1&api_key=RGAPI-4b544145-49c2-424d-9cc0-2bf92f56a5ca`
+      // );
+      // const matchData = await matchResponse.json();
+      // console.log(matchData);
 
-      const matchResponseTest = await fetch(
-        `https://europe.api.riotgames.com/lol/match/v5/matches/${matchData}?api_key=RGAPI-4b544145-49c2-424d-9cc0-2bf92f56a5ca`
-      );
-      const matchTestData = await matchResponseTest.json();
-      console.log(matchTestData);
+      // const matchResponseTest = await fetch(
+      //   `https://europe.api.riotgames.com/lol/match/v5/matches/${matchData}?api_key=RGAPI-4b544145-49c2-424d-9cc0-2bf92f56a5ca`
+      // );
+      // const matchTestData = await matchResponseTest.json();
+      // console.log(matchTestData);
     } catch (error) {
       console.error(error);
     }
